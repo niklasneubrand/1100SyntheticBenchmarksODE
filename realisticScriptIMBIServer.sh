9f8ace915679cedb3479f5def4686273884c265f
Niklas Neubrand <neubrand@imbi.uni-freiburg.de>
2024-06-10 13:59:03 +0200

    rename file (typo)

--- realisticScriptIMBIServer.sh
+++ realisticScriptIMBIServer.sh
@@ -3,7 +3,7 @@
 # define the directories
 script_dir=$(pwd)
 modelSet=fast10
-modelSet_dir=$script_dir/../Benchmark_Models/$modelSet
+modelSet_dir=$script_dir/Benchmark_Models/$modelSet
 
 echo "jobs submitted:"
 
@@ -16,15 +16,15 @@ for folder in $modelSet_dir/*/; do
     # Get the folder name without the path
     folder_name=$(basename "$folder")
 
-    # Skip the folders staring with a 0
-    if [[ $folder_name == 0* ]]; then
-        continue
-    fi
+    # Skip the folders staring with '0'
+    #if [[ $folder_name == 0* ]]; then
+    #    continue
+    #fi
 
     # Return the folder name
     echo "    "$folder_name
 
     # Collect Data by calling MATLAB script lhsLogging.m
-    nohup matlab-R2021a -r "initRealisticBenchmarks; cd('$folder_name'); arManyRealisticDesigns(); exit()" </dev/null >/dev/null 2>&1 &
+    nohup matlab-R2021a -r "initRealisticBenchmarks; cd('$folder'); arManyRealisticDesigns(1:20); exit()" </dev/null >/dev/null 2>&1 &
 
 done
