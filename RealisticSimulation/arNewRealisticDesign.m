64c4658dd202f874c1bea5f7b4d4f5ad7fb25cf6
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-06-12 12:29:46 +0200

    add plotting to the process

--- RealisticSimulation/arNewRealisticDesign.m
+++ RealisticSimulation/arNewRealisticDesign.m
@@ -125,6 +125,9 @@ try
     % Export the model to PEtab
     arExportPEtab();
 
+    % Plot the observables
+    arPlotFullPage();
+
     % clean up the project folder -> remove auxillary files
     movefile("SetupAuxillary.m", fullfile("Auxillary", "SetupAuxillary.m"));
 
