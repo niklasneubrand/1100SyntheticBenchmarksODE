0d3f5a1523d507e247755782a4f851dfa0cb8b18
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-07-02 12:42:34 +0200

    add scripts to distribute Isensee simulations

---
+++ simulateIsensee.sh
@@ -0,0 +1,11 @@
+# bash script to do the simulations of the Isensee model
+
+folder=$(pwd)
+folder="{$folder}/RS_IMBI/all_no_Chen_V1/Isensee_JCB2018"
+
+# lcreate a loop with index 1 to 50
+for i in {8..50}
+do
+    # Collect Data by calling MATLAB script lhsLogging.m
+    nohup matlab-R2021a -r "initRealisticBenchmarks; cd('$folder'); simulateIsensee($i:$i); exit();" </dev/null >/dev/null 2>&1 &
+done
