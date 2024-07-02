42cd2e7c7ee9bcf76ca5cb19e92b82056e2b0a81
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-07-02 16:19:09 +0200

    Simplify Becker model
    remove the Michaelis-Menten model. Otherwise RS package does not work. it expects a single ODE system.

--- BaseModels/all_no_Chen/Becker_Science2010/Setup.m
+++ BaseModels/all_no_Chen/Becker_Science2010/Setup.m
@@ -2,17 +2,22 @@
 
 arInit;
 
+% ODE model
 arLoadModel('epo_int_rep');
-arLoadModel('epo_binding');
 arLoadData('Epo_alpha_BaF3_Exp1_cpm_rep','epo_int_rep');
-arLoadData('Epo_binding_rep','epo_binding');
+
+% Michaelis-Menten model
+% deactivated for the realistic simulations
+% arLoadModel('epo_binding');
+% arLoadData('Epo_binding_rep','epo_binding');
 
 arCompileAll;
 
-% parameters with prior information
-arSetPars('init_Epo', log10(2100), 1, 1, -5, 4, 1, log10(2100), 0.5); % in older verions it was std=0.02 which was overwritten by arLoadPars by std=0.5
+% parameter with prior information
+% in older verions it was std=0.02 which was overwritten by arLoadPars by std=0.5
+arSetPars('init_Epo', log10(2100), 1, 1, -5, 4, 1, log10(2100), 0.5);
 
-% load best fit parameter values
+% load best parameters from a joint fit of ODE and Michaelis-Menten model
 arLoadPars('BestFit');
 
 % arPlot;
