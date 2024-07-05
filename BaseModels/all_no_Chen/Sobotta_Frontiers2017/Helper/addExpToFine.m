f540a30f82d5ee090e2dce70e59d54285d6a73e7
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-07-05 11:53:20 +0200

    rearange base models

--- BaseModels/all_no_Chen/Sobotta_Frontiers2017/Helper/addExpToFine.m
+++
@@ -1,9 +0,0 @@
-function addExpToFine()
-    global ar;
-    for m = 1 : length( ar.model )
-        for d = 1 : length( ar.model(m).data )
-            ar.model(m).data(d).tExtra = ar.model(m).data(d).tExp;
-        end
-    end
-    arLink;
-end
