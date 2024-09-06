4af46219588660ea8fe0d942c409175b128a0030
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-09-06 13:27:28 +0200

    replace arSimu by more robust heuristic
    instead of caling arSimu once, multiple ar.configs are tested until it works (or until number of nCVRestarts is reached).
    this approach is derived from heuristic in arClacMerit

--- project_lib/RealisticSimulation/arSimuMultiTries.m
+++ project_lib/RealisticSimulation/arSimuMultiTries.m
@@ -1,4 +1,4 @@
-function [simuSuccess, configSuccess, errReport] = arSimuMultiTries(sensi, fine, dynamics, options)
+function [simuSuccess, lastConfig, errReport] = arSimuMultiTries(sensi, fine, dynamics, options)
 
 arguments
     sensi (1,:) logical = true              % by default: true
@@ -24,7 +24,7 @@ qPositiveX = {ar.model(:).qPositiveX};
 %% set options for heuristic methods
 if isfield(options, 'nCVRestart')
     nCVRestart = options.nCVRestart;
-elseif isfield(ar.config, 'nCVRestart') || ~isnan(ar.config.nCVRestart)
+elseif isfield(ar.config, 'nCVRestart') && ~isnan(ar.config.nCVRestart)
     nCVRestart = ar.config.nCVRestart;
 else
     nCVRestart = 10;
@@ -96,10 +96,13 @@ for iTry = 1:nCVRestart
                         toggleVectorTols = 1;
                     elseif toggleVectorTols == 1
                         % switch back to original settings
-                        ar.config.atolV = atolV;
-                        ar.config.atolV_Sens = atolV_Sens;
+                        ar.config.atolV = configOld.atolV;
+                        ar.config.atolV_Sens = configOld.atolV_Sens;
                         toggleVectorTols = 2;
                         % instead: loosen tolerances
+                        increaseTols = 1;
+                    else
+                        % loosen tolerances
                         increaseTols = 1;
                     end
                 end
@@ -116,15 +119,14 @@ for iTry = 1:nCVRestart
                 arFprintf(1, 'Switch vector tolerance setting.\n')
             elseif toggleVectorTols == 2
                 arFprintf(1, 'Switch back to original vector tolerance setting.\n')
+                toggleVectorTols = 3;  % only show console output once
             end
             if increaseMaxsteps
-                % why 20%?
                 ar.config.maxsteps = options.stepsFactor*ar.config.maxsteps;
                 arFprintf(1, 'Increase maxsteps by factor %3.2f (now: %0.2e).\n', ...
                     options.stepsFactor, ar.config.maxsteps)
             end
             if increaseTols
-                % why 5%?
                 ar.config.atol = options.tolFactor*ar.config.atol;
                 ar.config.rtol = options.tolFactor*ar.config.rtol;
                 arFprintf(1, 'Increase tolerances by factor %3.2f (now: atol=%0.2e, rtol=%0.2e).\n', ...
@@ -135,14 +137,12 @@ for iTry = 1:nCVRestart
     end
 end
 
-% return the successful configurations
-configSuccess = struct();
-if simuSuccess && iTry > 1
-    configs = fieldnames(configOld);
-    for i = 1:length(configs)
-        if configOld.(configs{i}) ~= ar.config.(configs{i})
-            configSuccess.(configs{i}) = ar.config.(configs{i});
-        end
+% return the last configurations
+lastConfig = struct();
+configs = fieldnames(configOld);
+for i = 1:length(configs)
+    if configOld.(configs{i}) ~= ar.config.(configs{i})
+        lastConfig.(configs{i}) = ar.config.(configs{i});
     end
 end
 
