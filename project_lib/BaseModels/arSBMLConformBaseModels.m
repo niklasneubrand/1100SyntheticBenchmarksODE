ec117eb75560ce6bee462db12e34e2632bc5304a
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2024-09-13 14:24:56 +0200

    Cleanup Base Models
    * Bachmann, Becker, Swameye and Merkle now have SBML conform condition sections

---
+++ project_lib/BaseModels/arSBMLConformBaseModels.m
@@ -0,0 +1,51 @@
+function arSBMLConformBaseModels(modelSet)
+
+arguments
+    modelSet = 'fast'
+end
+
+global ar
+
+startDir = fileparts(mfilename('fullpath'));
+baseModelDir = fullfile(startDir, '..', '..', 'BaseModels');
+
+dirStruct = dir(fullfile(baseModelDir, modelSet));
+dirStruct = dirStruct([dirStruct.isdir]);
+dirStruct = dirStruct(~ismember({dirStruct.name}, {'.', '..'}));
+folders = {dirStruct.folder};
+names = {dirStruct.name};
+
+for ii = 1:length(folders)
+    cd(fullfile(folders{ii}, names{ii}));
+
+    qLoaded = arLoadLatest('normal');
+    if ~qLoaded
+        fprintf('Model %s not loaded\n', names{ii});
+        continue
+    end
+    
+    arUpdatePaths()
+
+    sbmlCompatible = arCheckSBMLCompatibilty(1);
+    if sbmlCompatible
+        fprintf('Model %s already SBML compatible\n', names{ii});
+        continue
+    end
+
+    fprintf('Converting model %s to be SBML compatible\n', names{ii});
+
+    targetDir = fullfile(folders{ii}, '..', sprintf('%s_SBML', modelSet), names{ii});
+    [~] = mkdir(targetDir);
+    try
+        diary(fullfile(targetDir, 'sbml_rename.log'));
+        arRenameModelCondPars(1, targetDir, false);
+        cd(targetDir);
+        arCompileAllSetups;
+        diary off
+    catch ME
+        fprintf('Error converting model %s: %s\n', names{ii}, ME.message);
+        diary off
+    end
+end
+
+end
---
+++ project_lib/BaseModels/arUpdatePaths.m
@@ -0,0 +1,30 @@
+function arUpdatePaths()
+% ARUPDATEPATHS updates model and data paths in the ar struct.
+% This can be useful after moving the project folder to a different location.
+
+global ar
+
+arInfoPathOld = ar.info.path;
+ar.info.path = pwd();
+
+for m = 1:length(ar.model)
+    mPathOld = ar.model(m).path;
+    mPathOldParts = split(mPathOld, arInfoPathOld);
+    if length(mPathOldParts) == 2
+        ar.model(m).path = fullfile(ar.info.path, mPathOldParts{2});
+    else
+        ar.model(m).path = fullfile(ar.info.path, 'Models');
+    end
+end
+
+for m = 1:length(ar.model)
+    for d = 1:length(ar.model(m).data)
+        dPathOld = ar.model(m).data(d).path;
+        dPathOldParts = split(dPathOld, arInfoPathOld);
+        if length(dPathOldParts) == 2
+            ar.model(m).data(d).path = fullfile(ar.info.path, dPathOldParts{2});
+        else
+            ar.model(m).data(d).path = fullfile(ar.info.path, 'Data');
+        end
+    end
+end
