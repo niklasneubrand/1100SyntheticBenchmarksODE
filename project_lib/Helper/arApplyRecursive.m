ae8d27cbf7043ed43d2c5614c8a19af6ed429f3e
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2025-02-21 23:07:42 +0100

    update helper scripts
    apply function to all d2d projects in folder

---
+++ project_lib/Helper/arApplyRecursive.m
@@ -0,0 +1,52 @@
+function reportTable = arApplyRecursive(func, folder, varargin, options)
+
+
+arguments
+    func (1,1) function_handle
+    folder (1,1) string = pwd()
+end
+
+% optional arguments for the function handle
+arguments (Repeating)
+    varargin
+end
+
+% options (Name-Value pairs)
+arguments
+    options.includeFolder (1,1) logical = true
+    options.requireCompiled (1,1) logical = false
+    options.reportName (1,:) char = ''
+end
+
+global ar
+
+startDir = cd(folder);
+d2dProjectFolders = arListProjectsRecursive(pwd(), options.includeFolder, options.requireCompiled);
+
+% loop through all projects
+for id = 1:length(d2dProjectFolders)
+    subDir = d2dProjectFolders{id};
+    [~, projectName] = fileparts(subDir);
+    projectName = string(projectName);
+    fprintf('Processing project: %s\n', projectName)
+    cd(subDir);
+    try
+        func(varargin{:});
+        success = true;
+        errorReport = "";
+    catch ME
+        success = false;
+        errorReport = string(getReport(ME, "extended", "hyperlinks", "off"));
+        display(getReport(ME, "extended", "hyperlinks", "on"));
+    end
+    reportTable(id, :) = table(projectName, success, errorReport);
+end
+
+cd(startDir)
+
+% save the report table
+if ~isempty(options.reportName)
+    save(options.reportName, 'reportTable')
+end
+
+end
--- project_lib/Helper/arListProjectsRecursive.m
+++ project_lib/Helper/arListProjectsRecursive.m
@@ -1,26 +1,38 @@
-function d2dProjectFolders = arListProjectsRecursive(folder, includeFolder)
+function d2dProjectFolders = arListProjectsRecursive(folder, includeFolder, requireCompiled)
 
 arguments
     folder (1,1) string = pwd()
     includeFolder (1,1) logical = true
+    requireCompiled (1,1) logical = false
 end
 
-subDirsRec =  split(genpath(folder), pathsep);  % get all subdirectories (recursive)
+% get char array of all subdirectories (recursive)
+subDirsRec =  split(genpath(folder), pathsep);
 subDirsRec = subDirsRec(1:end-1);  % remove last empty string
 % if desired, remove the folder itself
 if ~includeFolder
     subDirsRec = subDirsRec(2:end);
 end
 
-% find all folders with Models, Data and Results subfolders (i.e. d2d project folders)
+% find all d2d project folders (with or without compiled results)
 d2dProjectFolders = {};
 for id = 1:length(subDirsRec)
     subDir = subDirsRec{id};
-    if exist(fullfile(subDir, 'Models'), 'dir') && ...
-        exist(fullfile(subDir, 'Data'), 'dir') && ...
-        exist(fullfile(subDir, 'Results'), 'dir')
+
+    % skip d2d backup folders (they also have Models and Data folders)
+    isBackup = contains(subDir, 'Results') && contains(subDir, 'Backup');
+    if isBackup
+        continue
+    end
+
+    % check if the folder is a d2d project (and optionally compiled)
+    isD2DProject = exist(fullfile(subDir, 'Models'), 'dir') && ...
+        exist(fullfile(subDir, 'Data'), 'dir');
+    isCompiled = exist(fullfile(subDir, 'Results'), 'dir');
+
+    % append to list as required
+    if isD2DProject && (~requireCompiled || isCompiled)
         d2dProjectFolders = [d2dProjectFolders; subDir];
-        % arAllPlots(subDir);
     end
 end
 
