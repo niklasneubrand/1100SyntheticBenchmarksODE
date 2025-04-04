d40f35a2ac8a9c0dbc38fe029a1a8a39b59fa534
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2025-04-04 10:48:06 +0200

    add scripts to fix the DR data sets for all finished benchmarks

---
+++ analysisScripts/fixDoseResponses/fixDRall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Directory containing the synthetic benchmark folders
+base_dir="$(pwd)/../../SyntheticBenchmarks"
+echo "Processing base directory: $base_dir"
+
+# List of allowed directories
+has_DRdata_list=("Bachmann_MSB2011" "Brannmark_JBC2010" "Hass_PlosOne2017" "Isensee_JCB2018" "Lucarelli_CellSystems2017" "Merkle_PCB2016" "Sobotta_Frontiers2017")
+
+# Loop through all synthetic projects folders in the template directory
+for template_dir in "$base_dir"/*/; do
+    if [ -d "$template_dir" ]; then
+	
+        # Remove trailing slash from the template_dir path
+        template_dir="${template_dir%/}"
+        folder_name=$(basename "$template_dir")
+
+        # Check if the folder name is in the list of projects with DR data
+        if [[ ! " ${folder_name} " =~ " ${has_DRdata_list[@]} " ]]; then
+            echo "Skipping directory: $template_dir (no DR data -> needs no fix)"
+            continue
+        fi
+
+        echo "Processing directory: $template_dir"
+        
+        # Run the identifiability analysis in the background
+        nohup matlab-R2021a -r "diary(sprintf('recursiveDRfix_%s.log', '$folder_name')); fixDRrecursive('$template_dir'); diary('off'); exit;" </dev/null >/dev/null 2>&1 &
+    fi
+done
---
+++ analysisScripts/fixDoseResponses/fixDRrecursive.m
@@ -0,0 +1,19 @@
+function fixDRrecursive(folder)
+% FIXDRRECURSIVE Apply bug fix dose response data sets recursively to all projects in a directory
+
+% initialize the realistic simulations package
+startDir = pwd();
+cd('../..');
+initRealisticBenchmarks;
+cd(startDir);
+
+% get the name of the template from the directory
+[~, templateName] = fileparts(folder);
+outputName = sprintf('recursiveReport__fixDRdatasets__%s', templateName);
+
+% apply bug fix to all projects in the directory
+arApplyRecursive( ...
+    @fixDoseresponseDataSets, folder, ...
+    reportName=outputName, requireCompiled=false);
+
+end
