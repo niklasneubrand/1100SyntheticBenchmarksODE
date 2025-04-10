6935678c6e3a86a355ee4e1e06e5cd3e823b7e2d
Niklas Neubrand <38315848+niklasneubrand@users.noreply.github.com>
2025-04-10 21:41:49 +0200

    add function warpper for multiple projects

---
+++ project_lib/Helper/arApply2Projects.m
@@ -0,0 +1,66 @@
+function reportTable = arApply2Projects(func, folder, varargin, options)
+% ARAPPLY2PROJECTS Apply a function to multiple D2D projects in a directory
+%
+% INPUTS:
+%   func: function handle to be applied to each project
+%   folder: (optional) path to the directory containing the projects
+%          (default: pwd)
+%   varargin: additional arguments to be passed to the function
+%   options: Name-Value pairs for additional options
+%       method: Method to find project folders ('subfolders' or 'recursive', default: 'subfolders')
+%       includeFolder: Whether to include the folder itself in the search (default: true)
+%       requireCompiled: Whether to only include folders with compiled results (default: false)
+%       reportName: Name of the report file to save the results (default: 'report_arApply2Projects.mat')
+%
+% OUTPUTS:
+%   reportTable: Table containing the results of the function application
+
+arguments
+    func (1,1) function_handle
+    folder (1,1) string = pwd()
+end
+
+% optional arguments for the function handle
+arguments (Repeating)
+    varargin
+end
+
+% options (Name-Value pairs)
+arguments
+    options.method (1,1) string {mustBeMember(options.method, ["subfolders", "recursive"])} = "subfolders"
+    options.includeFolder (1,1) logical = true
+    options.requireCompiled (1,1) logical = false
+    options.reportName (1,:) char = 'report_arApply2Projects.mat'
+end
+
+global ar
+
+startDir = cd(folder);
+d2dProjectFolders = arListD2DProjects(pwd(), options.method, options.includeFolder, options.requireCompiled);
+
+% loop through all projects
+for id = 1:length(d2dProjectFolders)
+    subDir = d2dProjectFolders(id);
+    [~, projectName] = fileparts(subDir);
+    fprintf('Processing project: %s\n', projectName)
+    cd(subDir);
+    try
+        func(varargin{:});
+        success = true;
+        errorReport = "";
+    catch ME
+        success = false;
+        errorReport = string(getReport(ME, "extended", "hyperlinks", "off"));
+        display(getReport(ME, "extended", "hyperlinks", "on"));
+    end
+    reportTable(id, :) = table(string(projectName), string(subDir), success, errorReport);
+end
+
+cd(startDir)
+
+% save the report table
+if exist('reportTable', 'var') && ~isempty(options.reportName)
+    save(options.reportName, 'reportTable')
+end
+
+end
---
+++ project_lib/Helper/arListD2DProjects.m
@@ -0,0 +1,82 @@
+function d2dProjectFolders = arListD2DProjects(folder, method, includeFolder, requireCompiled)
+
+% todo:
+% - create the arApply function that generalizes the arApplyRecursive
+% function
+
+arguments
+    folder (1,:) string = pwd()
+    method (1,1) string {mustBeMember(method, ["subfolders", "recursive"])} = "subfolders"
+    includeFolder (1,1) logical = true
+    requireCompiled (1,1) logical = false
+end
+
+% if multiple folders are given, evaluate each folder and join results
+if length(folder) > 1
+    d2dProjectFolders = {};
+    for id = 1:length(folder)
+        addFolders = arListD2DProjects(folder(id), method, includeFolder, requireCompiled);
+        d2dProjectFolders = [d2dProjectFolders; addFolders];
+    end
+    return
+end
+
+% convert to absolute path
+folderDir = dir(folder);
+if isempty(folderDir) || ~folderDir(1).isdir
+    error('The given folder does not exist or is not a directory.')
+end
+folder = string(folderDir(1).folder);
+
+% get char array of all subdirectories (recursive)
+switch method
+    case "subfolders"
+        % get char array of all subdirectories (non-recursive)
+        subDirs = {folderDir([folderDir.isdir]).name}';
+        if includeFolder
+            % remove only '..'
+            subDirs = subDirs(~ismember(subDirs, {'..'}));
+        else
+            % remove '.' and '..'
+            subDirs = subDirs(~ismember(subDirs, {'.', '..'}));
+        end
+        % prepend the folder path to each subdirectory
+        subDirs = fullfile(folder, subDirs);
+    case "recursive"
+        % get char array of all subdirectories (recursive)
+        subDirs = split(genpath(folder), pathsep);
+        subDirs = subDirs(1:end-1);  % remove last empty string
+        % if desired, remove the folder itself
+        if ~includeFolder
+            subDirs = subDirs(2:end);
+        end
+    otherwise
+        error('Invalid method. Use "subfolders" or "recursive".')
+end
+subDirs = string(subDirs);
+
+
+% find all d2d project folders (with or without compiled results)
+d2dProjectFolders = [];
+for id = 1:length(subDirs)
+    subDir = subDirs(id);
+
+    % skip d2d backup folders (they also have Models and Data folders)
+    isBackup = contains(subDir, 'Results') && contains(subDir, 'Backup');
+    if isBackup
+        continue
+    end
+
+    % check if the folder is a d2d project (and optionally compiled)
+    isD2DProject = exist(fullfile(subDir, 'Models'), 'dir') && ...
+        exist(fullfile(subDir, 'Data'), 'dir');
+    isCompiled = exist(fullfile(subDir, 'Results'), 'dir');
+
+    % append to list as required
+    if isD2DProject && (~requireCompiled || isCompiled)
+        d2dProjectFolders = [d2dProjectFolders; subDir];
+    end
+end
+
+
+end
